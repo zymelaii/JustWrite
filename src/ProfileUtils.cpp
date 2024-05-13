35cb932b35b37aada7c60139b6c3c72015be570c
周上行Ryer <melaiiryer@gmail.com>
2024-05-13 18:47:02 +0800

    test: use us as time-unit of profiler

--- src/ProfileUtils.cpp
+++ src/ProfileUtils.cpp
@@ -1,6 +1,64 @@
 #ifndef NDEBUG
 
 #include "ProfileUtils.h"
+#include <QDebug>
+
+void Profiler::setup() {
+    timer_ = new QTimer(this);
+    timer_->setInterval(10000);
+    timer_->setSingleShot(false);
+    timer_->start();
+    QObject::connect(timer_, &QTimer::timeout, this, [this]() {
+        if (total_valid() == 0) { return; }
+        qDebug().noquote() << QStringLiteral("PROFILE DATA");
+        for (auto target : magic_enum::enum_values<ProfileTarget>()) {
+            const auto &data = profile_data_[indexof(target)];
+            if (data.empty()) { continue; }
+            qDebug().noquote() << QStringLiteral("  %1 %2us")
+                                      .arg(magic_enum::enum_name(target).data())
+                                      .arg(averageof(target));
+            clear(target);
+        }
+    });
+}
+
+void Profiler::start(ProfileTarget target) {
+    auto &rec = start_record_[indexof(target)];
+    if (rec.time_since_epoch().count() == 0) { rec = std::chrono::system_clock::now(); }
+}
+
+void Profiler::record(ProfileTarget target) {
+    const auto index = indexof(target);
+    auto      &rec   = start_record_[index];
+    if (rec.time_since_epoch().count() != 0) {
+        auto now = std::chrono::system_clock::now();
+        profile_data_[index].push_back(std::chrono::duration_cast<duration_t>(now - rec));
+        rec = timestamp_t{};
+    }
+}
+
+int Profiler::total_valid() const {
+    int count = 0;
+    for (auto data : profile_data_) {
+        if (!data.empty()) { ++count; }
+    }
+    return count;
+}
+
+int Profiler::indexof(ProfileTarget target) const {
+    return *magic_enum::enum_index(target);
+}
+
+float Profiler::averageof(ProfileTarget target) const {
+    const auto &data = profile_data_[indexof(target)];
+    float       sum  = 0;
+    for (auto dur : data) { sum += dur.count(); }
+    return sum / data.size();
+}
+
+void Profiler::clear(ProfileTarget target) {
+    profile_data_[indexof(target)].clear();
+}
 
 Profiler JwriteProfiler;
 
--- src/ProfileUtils.h
+++ src/ProfileUtils.h
@@ -2,7 +2,6 @@
 
 #include <QList>
 #include <QTimer>
-#include <QDebug>
 #include <chrono>
 #include <array>
 #include <magic_enum.hpp>
@@ -14,6 +13,7 @@
 #endif
 
 #ifndef NDEBUG
+
 enum class ProfileTarget {
     IME2UpdateDelay,
     FrameRenderCost,
@@ -22,74 +22,30 @@ enum class ProfileTarget {
     SwitchChapter,
 };
 
-struct Profiler : public QObject {
+class Profiler : public QObject {
+public:
     using timestamp_t     = decltype(std::chrono::system_clock::now());
-    using duration_t      = std::chrono::milliseconds;
+    using duration_t      = std::chrono::microseconds;
     using duration_list_t = QList<duration_t>;
     using start_record_t  = std::array<timestamp_t, magic_enum::enum_count<ProfileTarget>()>;
     using profile_data_t  = std::array<duration_list_t, magic_enum::enum_count<ProfileTarget>()>;
 
-    start_record_t start_record;
-    profile_data_t profile_data;
-    QTimer        *timer = nullptr;
+    void setup();
+    void start(ProfileTarget target);
+    void record(ProfileTarget target);
 
-    int indexof(ProfileTarget target) {
-        return *magic_enum::enum_index(target);
-    }
+protected:
+    int   total_valid() const;
+    int   indexof(ProfileTarget target) const;
+    float averageof(ProfileTarget target) const;
+    void  clear(ProfileTarget target);
 
-    void start(ProfileTarget target) {
-        auto &rec = start_record[indexof(target)];
-        if (rec.time_since_epoch().count() == 0) { rec = std::chrono::system_clock::now(); }
-    }
-
-    void record(ProfileTarget target) {
-        const auto index = indexof(target);
-        auto      &rec   = start_record[index];
-        if (rec.time_since_epoch().count() != 0) {
-            auto now = std::chrono::system_clock::now();
-            profile_data[index].push_back(std::chrono::duration_cast<duration_t>(now - rec));
-            rec = timestamp_t{};
-        }
-    }
-
-    int total_valid() {
-        int count = 0;
-        for (auto data : profile_data) {
-            if (!data.empty()) { ++count; }
-        }
-        return count;
-    }
-
-    float averageof(ProfileTarget target) {
-        const auto &data = profile_data[indexof(target)];
-        float       sum  = 0;
-        for (auto dur : data) { sum += dur.count(); }
-        return sum / data.size();
-    }
-
-    void clear(ProfileTarget target) {
-        profile_data[indexof(target)].clear();
-    }
-
-    void setup() {
-        timer = new QTimer(this);
-        timer->setInterval(10000);
-        timer->setSingleShot(false);
-        timer->start();
-        QObject::connect(timer, &QTimer::timeout, this, [this]() {
-            if (total_valid() == 0) { return; }
-            qDebug().noquote() << QStringLiteral("PROFILE DATA");
-            for (auto target : magic_enum::enum_values<ProfileTarget>()) {
-                const auto &data = profile_data[indexof(target)];
-                if (data.empty()) { continue; }
-                qDebug().noquote() << QStringLiteral("  %1 %2ms")
-                                          .arg(magic_enum::enum_name(target).data())
-                                          .arg(averageof(target));
-                clear(target);
-            }
-        });
-    }
+private:
+    start_record_t start_record_;
+    profile_data_t profile_data_;
+    QTimer        *timer_ = nullptr;
 };
 
 extern Profiler JwriteProfiler;
+
 #endif
